d63279fa70e9da5199bd5f266cad10e8cfc89c03
Eyob Mengistu <emengistu17@gmail.com>
2025-06-22 21:28:54 -0500

    Implement VehicleController with full CRUD

--- src/main/java/com/pluralsight/dealership_api/controllers/VehiclesController.java
+++ src/main/java/com/pluralsight/dealership_api/controllers/VehiclesController.java
@@ -1,8 +1,47 @@
 package com.pluralsight.dealership_api.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.pluralsight.dealership_api.dao.VehicleDao;
+import com.pluralsight.dealership_api.dealership.Vehicle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/vehicles")
 public class VehiclesController {
-    
+
+    private VehicleDao vehicleDao;
+
+    @Autowired
+    public VehiclesController(VehicleDao vehicleDao) {
+        this.vehicleDao = vehicleDao;
+    }
+
+    // CREATE
+    @PostMapping
+    public void addVehicle(@RequestBody Vehicle vehicle) {
+        vehicleDao.add(vehicle);
+    }
+
+    // READ ALL
+    @GetMapping
+    public List<Vehicle> getAllVehicles() {
+        return vehicleDao.getAll();
+    }
+
+    // UPDATE by VIN
+    @PutMapping("/{vin}")
+    public void updateVehicle(@PathVariable String vin, @RequestBody Vehicle vehicle) {
+        vehicle.setVin(vin); // Ensure VIN matches path
+        vehicleDao.update(vehicle);
+
+    }
+
+    // DELETE by VIN
+    @DeleteMapping("/{vin}")
+    public void deleteVehicle(@PathVariable String vin) {
+        vehicleDao.delete(vin);
+    }
+
 }
